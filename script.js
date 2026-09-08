ff6bac4f3a6c9b0547c0ca62d3fec7e4e96ea16b
3127713368-source <3127713368@qq.com>
2026-09-08 20:00:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,124 @@
 let resultImage = "";
 
 
-
 document
 .getElementById("upload")
 .addEventListener(
 "change",
 function(){
 
-let file=this.files[0];
+    let file = this.files[0];
 
+    let img = document.getElementById(
+        "imagePreview"
+    );
 
-let img=
-document.getElementById(
-"imagePreview"
-);
-
-
-img.src=
-URL.createObjectURL(file);
-
-
-resultImage=img.src;
-
+    img.src = URL.createObjectURL(file);
 
 });
 
 
 
-function processImage(){
+async function processImage(){
 
-alert(
-"AI正在处理中...\n\n正式版本会连接AI去水印系统"
-);
+    let fileInput = document.getElementById("upload");
+
+
+    if(!fileInput.files[0]){
+
+        alert("请先上传图片");
+
+        return;
+
+    }
+
+
+    let file = fileInput.files[0];
+
+
+    let formData = new FormData();
+
+    formData.append(
+        "image",
+        file
+    );
+
+
+    alert("LZY AI 正在处理中...");
+
+
+    try{
+
+
+        let response = await fetch(
+            "/.netlify/functions/remove-watermark",
+            {
+
+                method:"POST",
+
+                body:formData
+
+            }
+        );
+
+
+        let result = await response.json();
+
+
+        alert(result.message);
+
+
+
+    }
+
+    catch(error){
+
+
+        alert(
+            "AI连接失败"
+        );
+
+
+        console.log(error);
+
+
+    }
+
 
 }
+
 
 
 
 function downloadImage(){
 
-if(resultImage){
 
-let a=document.createElement("a");
-
-a.href=resultImage;
-
-a.download="LZY-result.png";
-
-a.click();
+    if(resultImage){
 
 
-}
+        let a=document.createElement("a");
 
-else{
 
-alert(
-"请先上传图片"
-);
+        a.href=resultImage;
 
-}
+
+        a.download="LZY-result.png";
+
+
+        a.click();
+
+
+    }
+
+
+    else{
+
+
+        alert(
+            "请先处理图片"
+        );
+
+
+    }
 
 }
